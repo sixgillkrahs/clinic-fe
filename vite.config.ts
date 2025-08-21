4db88fdf7686345a3ce19a16117919b64d308c46
sixgillkrahs <dvq2804@gmail.com>
2025-08-21 23:09:53 +0700

    setup base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
